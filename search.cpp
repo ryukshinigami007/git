a0cd40b06e986e03711e52d134e7a824ba5d6b71
ryukshinigami007 <109077805+ryukshinigami007@users.noreply.github.com>
2022-10-14 21:20:03 +0530

    Create search.cpp
    Given 3 numbers N , L and R. Print 'yes' if N is between L and R else print 'no'.

---
+++ search.cpp
@@ -0,0 +1,27 @@
+/*
+Given 3 numbers N , L and R. Print 'yes' if N is between L and R else print 'no'.
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int key;
+    cin >> key;
+
+    int s , e;
+    cin >> s >> e ;
+    for(int i =s;i<=e;i++)
+    {
+        if(i == key)
+        {
+            cout << "yes";
+            break;
+        }
+        else if (i==e&&i!=key)
+        {
+            cout << "no";
+        }
+    }
+    return 0;
+}
